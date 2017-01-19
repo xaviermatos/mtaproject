4c5acf00724e56be3e858320f5e9b59babf65768
xaviermatos <xmatos@aoiths.org>
2017-01-19 23:15:55 +0000

    added arrays

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
 // JavaScript File
+/*global $*/
 $(document).ready(function() {
-
-          $("#btn").click(function(){
-         var input3 = $("#input1").val();
+ $("#btn").click(function(){
+  var input3 = $("#input1").val();
           
-          $.getJSON(
-     
-      "https://api.giphy.com/v1/gifs/search?q="+ input3 + "&api_key=dc6zaTOxFJmzC",
-      "https://ajar-target.gomix.me/status"
+      $.getJSON( "https://ajar-target.gomix.me/status" , function (response) {
+       console.log(response)
+       var stat = (response)
+       $("body").append(response[1].status);
+      var trains = ["1","2","3","4","6","7","a","b","c","d","e","f","g","j","l","m","n","q","r","s","w","z"
+      ];
+      var arrayLength = trains.length;
+     for (var i = 0;i < arrayLength; i++) {
+      if(input3 === trains[key])
+      alert(mystringArray[i])
+      }
+       
+});
+      
+   /*
       function(response) {
         console.log(response);
         $("#gif").append("<img src=" + response.data[0].images.original.url + ">");
@@ -23,4 +34,6 @@ $(document).ready(function() {
      $("#input3").remove();
      $("#gif").hide();
  })
+ 
+ */
 });
